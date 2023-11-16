4708bcb480fa36b551dea507bee97020dd0a4408
Venkatesh Sekaran <99889574+venkateshsekaran@users.noreply.github.com>
2023-11-16 13:03:08 +0530

    Update Contact.jsx

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -28,7 +28,7 @@ function Contact() {
         "service_iwdkhum",
         "template_574n8bj",
         event.target,
-        "c6lUpDb-MG8FKGFrb"
+        "5Pb62QrAvipQA5YG9p50-"
       )
       .then((res) => {
         console.log(res);
